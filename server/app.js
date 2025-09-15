b6d610e88006d461a0da099f5e4dd51df7dbc1a3
deyaa2 <115751101+deyaa2@users.noreply.github.com>
2025-09-15 15:18:52 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -46,7 +46,7 @@ try {
 (async () => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb+srv://deaa16zed21_db_user:TZ1p3Af5Vk4XNtj6@deyaacluster.zoadiv2.mongodb.net/test?retryWrites=true&w=majority&appName=DeyaaCluster',
+      process.env.MONGO_URI,
       { useNewUrlParser: true, useUnifiedTopology: true }
     );
     console.log('✅ MongoDB connected');
